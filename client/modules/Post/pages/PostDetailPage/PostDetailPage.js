701a05c5fec7a2b0a57ece110bae6ce1c1830d80
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-08 17:25:00 +0100

    edit components

--- client/modules/Post/pages/PostDetailPage/PostDetailPage.js
+++ client/modules/Post/pages/PostDetailPage/PostDetailPage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
+import { injectIntl, FormattedMessage } from 'react-intl';
+//injectIntl(funkcja, która "wstrzykuje"do komponentu tłumaczeniedlawiadomości(???))
 
 // Import Style
 import styles from '../../components/PostListItem/PostListItem.css';
@@ -13,18 +14,39 @@ import { fetchPost } from '../../PostActions';
 // Import Selectors
 import { getPost } from '../../PostReducer';
 
-export function PostDetailPage(props) {
-  return (
-    <div>
-      <Helmet title={props.post.title} />
-      <div className={`${styles['single-post']} ${styles['post-detail']}`}>
-        <h3 className={styles['post-title']}>{props.post.title}</h3>
-        <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
-        <p className={styles['post-desc']}>{props.post.content}</p>
+export class PostDetailPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.post.name,
+      title: this.props.post.title,
+      content: this.props.post.content,
+    };
+  }
+  //metoda obsługująca zmiany zachodzące w każdym z pól formularza
+  handleInputChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+  const {props} = this;
+    return (
+      <div>
+        <Helmet title={props.post.title} />
+          <div className={`${styles['single-post']} ${styles['post-detail']}`}>
+          <h3 className={styles['post-title']}>{props.post.title}</h3>
+          <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
+          <p className={styles['post-desc']}>{props.post.content}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }  
 }
+
 
 // Actions required to provide data for this component to render in server side.
 PostDetailPage.need = [params => {
