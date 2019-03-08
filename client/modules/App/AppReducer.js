8a0d6940c12f09276718fb8f52c08de3acf1dc6d
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-08 17:53:37 +0100

    edit postdetailpage

--- client/modules/App/AppReducer.js
+++ client/modules/App/AppReducer.js
@@ -1,5 +1,7 @@
 // Import Actions
 import { TOGGLE_ADD_POST, TOGGLE_EDIT_POST } from './AppActions';
+import { fetchPost, editPostRequest } from '../../PostActions';
+import { toggleEditPost } from '../../../App/AppActions';
 
 // Initial State
 const initialState = {
